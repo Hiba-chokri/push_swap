78e8540ef263ad0af86299f2b61ff082c9c11254
Hiba Chokri <hichokri@e2r1p8.1337.ma>
2024-03-16 17:21:23 +0000

    construction phase

---
+++ includes/functions.c
@@ -0,0 +1,100 @@
+#include "push_swap.h"
+
+// functions that will be used in filling the
+typedef struct s_list
+{
+    void	*content;
+    struct s_list	*next;
+}				t_list;
+
+int	ft_atoi(const char *nptr)
+{
+	int	i;
+	int	sign;
+	int	res;
+
+	i = 0;
+	sign = 1;
+	res = 0;
+	while (nptr[i] && (nptr[i] == ' ' || (nptr[i] >= '\t' && nptr[i] <= '\r')))
+		i++;
+	if (nptr[i] == '-')
+	{
+		sign *= -1;
+		i++;
+	}
+	else if (nptr[i] == '+')
+		i++;
+	while (nptr[i] != '\0' && (nptr[i] >= '0' && nptr[i] <= '9'))
+	{
+		res *= 10;
+		res += nptr[i] - '0';
+		i++;
+	}
+	return (res * sign);
+}
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new_node;
+
+	new_node = NULL;
+	new_node = (t_list *)malloc(sizeof(t_list));
+	if (!new_node)
+		return (NULL);
+	new_node->content = content;
+	new_node->next = NULL;
+	return (new_node);
+}
+t_list	*ft_lstlast(t_list *lst)
+{
+	t_list	*ptr;
+
+	if (!lst)
+		return (NULL);
+	ptr = lst;
+	while (ptr->next != NULL)
+		ptr = ptr->next;
+	return (ptr);
+}
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	if (lst == NULL)
+		return ;
+	if (*lst == NULL)
+		*lst = new;
+    	else
+		ft_lstlast(*lst)->next = new;
+}
+
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	if (lst == NULL || new == NULL)
+		return ;
+	new->next = *lst;
+	*lst = new;
+}
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*tmp;
+
+	if (!lst || !*lst || !del)
+		return ;
+	tmp = *lst;
+	while (tmp)
+	{
+		tmp = tmp->next;
+		ft_lstdelone(*lst, del);
+		*lst = tmp;
+	}
+}
+
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	if (!lst || !del)
+		return ;
+	del(lst->content);
+	free(lst);
+}
---
+++ includes/push_swap.c
@@ -0,0 +1,28 @@
+#include "push_swap.h"
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+// steps
+//create a structrure that will represent the stack
+typedef struct s_stack
+{
+    int *data;
+    struct s_stack *next;
+} t_stack;
+//create a pointer to the top of the stack
+t_stack *top = 0;
+// fill the stack with the args values
+void fill_stack(t_stack *stack, int argc, char *argv[])
+{
+    t_list *tmp;
+
+    top = lstnew()
+}
+int main(int argc , char *argv[])
+{
+    while (i < argc)
+    {
+
+    }
+}
